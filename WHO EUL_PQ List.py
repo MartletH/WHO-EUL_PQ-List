e500028ab89192660f32dc891840d2c649c1c159
Martlet <21070822+DarcyCJ@users.noreply.github.com>
2021-05-09 22:21:54 +0800

    Create WHO EUL_PQ List.py
    Initial version. Tested working.

---
+++ WHO EUL_PQ List.py
@@ -0,0 +1,17 @@
+import requests
+import re
+from os import path
+
+# get response from WHO
+url = 'https://extranet.who.int/pqweb/key-resources/documents/status-covid-19-vaccines-within-who-eulpq-evaluation-process'
+r = requests.get(url)
+
+# Extract filename with re from url
+filename = re.findall('(?:.+\/)(.+)',r.url)[0]
+
+# Save file if there's latest version
+if path.exists( filename):
+    print('Latest verson already in folder. Please come back several days later!')
+else:
+    with open(filename, 'wb') as f:
+        f.write(r.content)
